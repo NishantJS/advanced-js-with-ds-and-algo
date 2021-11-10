ca736645cd6d057a8503e0fbf877ae7982c13bcd
Nishant Chorge <nishantchorge79@gmail.com>
2021-11-10 21:32:14 +0530

    Reversing Array

---
+++ ds/array/reverseArray.js
@@ -0,0 +1,23 @@
+const reverseUsingMap=(arr=[])=> {
+  return arr.map((element, index) => arr[arr.length - 1 - index]);
+}
+
+const reverse = (arr=[]) => {
+  let rev = [];
+  for (let i = arr.length - 1; i >= 0; i--){
+    rev.push(arr[i]);
+  }
+  return rev;
+}
+
+const generateArray = () => {
+  let arr = [];
+  let no_of_elements = Math.round(Math.random() * 5 + 5);
+  for (let i = 0; i < no_of_elements; i++) {
+    arr.push(Math.round(Math.random() * 10));
+  }
+  return arr;
+}
+
+let arr = generateArray();
+console.log({ arr: arr,rev: reverseUsingMap(arr), rev2: reverse(arr) });
